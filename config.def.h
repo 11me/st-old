0f5ec6a832e5462fc375f26599382665071926d5
lime <osmium760@gmail.com>
2021-01-21 22:03:31 +0300

    add colors

--- config.def.h
+++ config.def.h
@@ -114,37 +114,38 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.7;
+float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
+
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#313539", /* black   */
+	"#b02626", /* red     */
+	"#40a62f", /* green   */
+	"#f2e635", /* yellow  */
+	"#314ad0", /* blue    */
+	"#b30ad0", /* magenta */
+	"#32d0fc", /* cyan    */
+	"#acadb1", /* white   */
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#676f78", /* black   */
+	"#b55454", /* red     */
+	"#78a670", /* green   */
+	"#faf380", /* yellow  */
+	"#707fd0", /* blue    */
+	"#c583d0", /* magenta */
+	"#8adaf1", /* cyan    */
+	"#e0e3e7", /* white   */
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"black",
-  "#282c34",
+  "#add8e6", /* 256 -> cursor */
+	"#555555", /* 257 -> rev cursor*/
+	"#282828", /* 258 -> bg */
+	"#ebdbb2", /* 259 -> fg */
 };
 
 
@@ -152,7 +153,7 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
+unsigned int defaultfg = 256;
 unsigned int defaultbg = 258;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
