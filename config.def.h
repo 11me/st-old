6f0ec4799413517d45b59b40e4308a0574e73acc
lime <osmium760@gmail.com>
2021-01-30 20:08:48 +0300

    change bg color

--- config.def.h
+++ config.def.h
@@ -145,7 +145,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
   "#add8e6", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor*/
-	"#282828", /* 258 -> bg */
+	"#282C34", /* 258 -> bg */
 	"#ebdbb2", /* 259 -> fg */
 };
 
